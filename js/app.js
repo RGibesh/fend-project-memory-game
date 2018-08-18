38c78859eb491653f58a590646c2edb2750d851e
Daniel Otieno <danielotieno.ke@gmail.com>
2018-08-18 13:31:17 +0300

    Add model color

--- js/app.js
+++ js/app.js
@@ -230,10 +230,10 @@ function rating() {
     if (moves < max) {
         starRate = "<i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star'></i>";
     } else if (moves < min) {
-        stars[2].style.color = "#444";
+        stars[2].style.color = "#ffb400";
         starRate = "<i class='fa fa-star'></i><i class='fa fa-star'></i>";
     } else {
-        stars[1].style.color = "#444";
+        stars[1].style.color = "#ffb400";
         starRate = "<i class='fa fa-star'></i>";
     }
 
